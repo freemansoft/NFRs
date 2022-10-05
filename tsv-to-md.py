1e4e37c7317d93d2562d9c449364e337a203054f
Joe Freeman <joe@freemansoft.com>
2022-10-05 15:57:51 -0400

    Make the conversion script general purpose with command line parameter.

--- tsv-to-md.py
+++ tsv-to-md.py
@@ -1,8 +1,39 @@
 import pandas as pd
+import argparse
 
-csvFile = "NFRs.tsv"
-mdFile = "NFRs.md"
-df = pd.read_csv(csvFile, engine="python", sep="\t", header=0)
-with open(mdFile, "w") as md:
+csvFileDefault = "NFRs.tsv"
+mdFileDefault = "NFRs.md"
+headerRowDefault = 0
+
+parser = argparse.ArgumentParser(description="Convert tabular data to markdown tables.")
+parser.add_argument(
+    "--source",
+    dest="csvFile",
+    default=csvFileDefault,
+    help="The delimited source file [" + csvFileDefault + "]",
+)
+parser.add_argument(
+    "--dest",
+    dest="mdFile",
+    default=mdFileDefault,
+    help="The target md file [" + mdFileDefault + "]",
+)
+parser.add_argument(
+    "--sep",
+    dest="sep",
+    default="\t",
+    help="Source column separator [tab]",
+)
+parser.add_argument(
+    "--header",
+    dest="header",
+    default=headerRowDefault,
+    help="The header row [" + str(headerRowDefault) + "]",
+)
+
+args = parser.parse_args()
+
+df = pd.read_csv(args.csvFile, engine="python", sep=args.sep, header=args.header)
+with open(args.mdFile, "w") as md:
     df.fillna("", inplace=True)
     df.to_markdown(buf=md, index=False)
